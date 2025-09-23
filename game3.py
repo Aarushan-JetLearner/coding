45c9d1404c8fb425bf16f57a97b0082f2eb97f5b
Aarushan-JetLearner <taarushan@gmail.com>
2025-09-23 19:34:35 +0100

    Add files via upload

--- game3.py
+++ game3.py
@@ -1,10 +1,11 @@
 import pgzrun,random
 WIDTH=1000
-HEIGHT=1000
+HEIGHT=730
 TITLE="flying bee"
 actor=Actor("flower")
 actor2=Actor("bee")
 score=0
+game_over=False
 
 actor.pos=100,100
 
@@ -14,7 +15,12 @@ def draw():
     screen.blit("sky (1)",(0,0))
     actor.draw()
     actor2.draw()
-    screen.draw.text("score="+str(score),color="black",fontsize=25,topleft=(50,50))
+    
+    global game_over
+    if game_over==True:
+        screen.blit("black screen",(0,0))
+        screen.draw.text("Your score is "+str(score),color="white",fontsize=25,topleft=(50,50))
+        
 def update():
     global score
     if keyboard.left:
@@ -27,6 +33,13 @@ def update():
         actor2.y=actor2.y-10
     if actor2.colliderect(actor):
         actor.x=random.randint(0,1000)
-        actor.y=random.randint(0,1000)
+        actor.y=random.randint(0,730)
         score=score+10
+
+def timer():
+    print("timer")
+    global game_over
+    game_over=True
+clock.schedule(timer,60)
+
 pgzrun.go()
