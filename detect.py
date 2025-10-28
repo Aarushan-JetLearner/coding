3077f97e81bbcc660ac15ada2f59119b8e55a21e
Aarushan-JetLearner <taarushan@gmail.com>
2025-10-28 19:05:27 +0000

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,8 @@
+
+def var():
+    string={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+question=input("Enter a sentence")
+if "a"and"b"and"c"and"d"and"e"and"f"and"g"and"h"and"i"and"j"and"k"and"l"and"m"and"n"and"o"and"p"and"q"and"r"and"s"and"t"and"u"and"v"and"w"and"x"and"y"and"z" in question:
+    print("Pangram")
+else:
+    print("Not a pangram")
