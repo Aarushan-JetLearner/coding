62e9ae25bab3f1239152ec92336d22f04f3fb7e2
Aarushan-JetLearner <taarushan@gmail.com>
2025-11-11 19:25:01 +0000

    Add files via upload

--- setgame2.py
+++ setgame2.py
@@ -1,7 +1,7 @@
 import pgzrun,random
 WIDTH=1000
 HEIGHT=1000
-items=["blackre","bluer","whiter","redr","greenr","background colors"]
+items=["black","blue","white","red"]
 items_list=[]
 animation=[]
 game_over=False
@@ -10,7 +10,7 @@ number_of_levels=7
 initial_speed=10
 current_level=1
 def draw():
-    screen.blit("background colors",(0,0))
+    screen.blit("bground",(0,0))
     if game_over:
         screen.draw.text("You lose!",fontsize=60,center=(500,100),color="white")
     elif game_complete:
@@ -23,8 +23,40 @@ def update():
     if len(items_list)==0:
         items_list=update_level(current_level)
 def update_level(current_level):
-    item2=["greenr"]
+    item2=["green"]
     for i in range(current_level):
         variable=random.choice(items)
         item2.append(variable)
+    emptylistactors=[]
+    for i in item2:
+        item=Actor(i+"r")
+        emptylistactors.append(item)
+        
+    arrange_item(emptylistactors)
+    animate_items(emptylistactors)
+    return emptylistactors
+def arrange_item(items_list):
+    no_gaps=len(items_list)+1
+    gap_size=1000/no_gaps
+    for i,item in enumerate(items_list):
+        item.x=(i+1)*gap_size
+def animate_items(items_list):
+    global animation
+    for i in items_list:
+        time_dur=initial_speed-current_level
+        i.anchor=("center", "bottom")
+        a=animate(i,duration=time_dur,on_finished=handle_game_over,y=1000)
+        animation.append(a)
+def handle_game_over():
+    global game_over
+    game_over=True
+def on_mouse_down(pos):
+    for i in items_list:
+        if i.collidepoint(pos):
+            if "green" in i.image:
+                handle_game_complete
+            else:
+                handle_game_over
+def handle_game_complete():
+    pass
 pgzrun.go()
