50fda06a506a026753deae12aee51f17efdaad7e
Aarushan-JetLearner <taarushan@gmail.com>
2025-11-25 20:00:51 +0000

    Add files via upload

--- galaga.py
+++ galaga.py
@@ -2,8 +2,15 @@ import pgzrun,random
 WIDTH=1000
 HEIGHT=500
 actor1=Actor("galaga")
-actor2=Actor("bug")
+enemiess=[]
+direction=1
+for i in range(8):
+    actor2=Actor("bug")
+    enemiess.append(actor2)
+    enemiess[-1].x=100+90*i
+    enemiess[-1].y=-50
 actor1.pos=(500,450)
+score=0
 def draw():
     screen.fill("black")
     actor1.draw()
@@ -11,7 +18,10 @@ def draw():
         i.draw()
     for i in enemies:
         i.draw()
+    screen.draw.text(str(score),(0,0))
 def update():
+    global direction
+    move_down=False
     if keyboard.left:
         if actor1.x>0:
             actor1.x=actor1.x-10
@@ -37,13 +47,24 @@ def update():
             i.y=i.y-15
         else:
             bullets.remove(i) 
-    for i in enemies:
-        if i.y<=500:
-            i.y=i.y+5
-        else:
-            i.y=-50
-            i.x=random.randint(50,950)
+    if enemiess and(enemiess[0].x<0 or enemiess[-1].x>1000):
+        move_down=True
+        direction=direction*-1
 
+    for i in enemies:
+        i.x=i.x+5*direction
+        if move_down==True:
+            i.y=i.y+50
+            for j in bullets:
+                global score
+                if i.colliderect(j):
+                    
+                    bullets.remove(j)
+                    enemies.remove(i)
+                    score=score+1
+
+
+    
         
      
     
@@ -53,6 +74,7 @@ def on_key_down(key):
       bullets.append(Actor("bullet"))
       bullets[-1].x=actor1.x
       bullets[-1].y=actor1.y-50
+      sounds.eep.play()
 
 enemies=[]
 enemies.append(actor2)
