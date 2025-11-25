2be7e6d681f125325714b2db2bf859e6616e7360
Aarushan-JetLearner <taarushan@gmail.com>
2025-11-25 19:08:46 +0000

    Add files via upload

---
+++ spaceship.py
@@ -0,0 +1,58 @@
+import pgzrun,random
+WIDTH=1000
+HEIGHT=500
+actor1=Actor("spaceshipp")
+actor2=Actor("aliennn")
+actor1.pos=(500,450)
+def draw():
+    screen.fill("black")
+    actor1.draw()
+    for i in bullets:
+        i.draw()
+    for i in enemies:
+        i.draw()
+def update():
+    if keyboard.left:
+        if actor1.x>0:
+            actor1.x=actor1.x-10
+        else:
+            actor1.x=actor1.x
+    if keyboard.right:
+        if actor1.x<1000:
+            actor1.x=actor1.x+10
+        else:
+            actor1.x=actor1.x
+    if keyboard.up:
+        if actor1.y>0:
+            actor1.y=actor1.y-10
+        else:
+            actor1.y=actor1.y
+    if keyboard.down:
+        if actor1.y<500:
+            actor1.y=actor1.y+10
+        else:
+            actor1.y=actor1.y
+    for i in bullets:
+        if i.y>=0:
+            i.y=i.y-15
+        else:
+            bullets.remove(i)
+    for i in enemies:
+        if i.y<=500:
+            i.y=i.y+5
+        else:
+            i.y=-50
+            i.x=random.randint(50,950)
+bullets=[]
+def on_key_down(key):
+    if key==keys.SPACE:
+        bullets.append(Actor("bullet"))
+        bullets[-1].x=actor1.x
+        bullets[-1].y=actor1.y-50
+
+
+enemies=[]
+enemies.append(actor2)
+enemies[-1].x=500
+enemies[-1].y=0
+pgzrun.go()
