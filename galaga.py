09f751ca297a10391faad928467d9eb9946a4a5c
Aarushan-JetLearner <taarushan@gmail.com>
2025-12-02 19:35:55 +0000

    Add files via upload

--- galaga.py
+++ galaga.py
@@ -4,6 +4,7 @@ HEIGHT=500
 actor1=Actor("galaga")
 enemiess=[]
 direction=1
+game_over=False
 for i in range(8):
     actor2=Actor("bug")
     enemiess.append(actor2)
@@ -12,6 +13,8 @@ for i in range(8):
 actor1.pos=(500,450)
 score=0
 def draw():
+    global game_over
+    screen.clear()
     screen.fill("black")
     actor1.draw()
     for i in bullets:
@@ -19,9 +22,19 @@ def draw():
     for i in enemiess:
         i.draw()
     screen.draw.text(str(score),(0,0))
+    if game_over==True:
+        screen.draw.text("Game over!",fontsize=60,center=(500,50))
 def update():
     global direction
     move_down=False
+    for i in enemiess:
+        for j in bullets:
+                global score
+                if i.colliderect(j):
+                    
+                    bullets.remove(j)
+                    enemiess.remove(i)
+                    score=score+1
     if keyboard.left:
         if actor1.x>0:
             actor1.x=actor1.x-10
@@ -50,19 +63,15 @@ def update():
     if enemiess and(enemiess[0].x<0 or enemiess[-1].x>1000):
         move_down=True
         direction=direction*-1
+    if len(enemiess)==0:
+        global game_over
+        
+        game_over=True
 
     for i in enemiess:
         i.x=i.x+5*direction
         if move_down==True:
             i.y=i.y+50
-            for j in bullets:
-                global score
-                if i.colliderect(j):
-                    
-                    bullets.remove(j)
-                    enemiess.remove(i)
-                    score=score+1
-
 
     
         
