d9fa42bf83f5dce1d06ca4cb9a0d02c140aeb12e
Aarushan-JetLearner <taarushan@gmail.com>
2025-12-30 21:36:25 +0530

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@ import pgzrun,random
 import pgzrun,time
 WIDTH=1000
 HEIGHT=500
+score=0
 Welcome=Rect(0,0,800,60)
 Question=Rect(5,75,500,150)
 Welcome=Rect(0,0,1000,60)
@@ -12,6 +13,7 @@ Option1=Rect(0,240,400,125)
 Option2=Rect(425,240,400,125)
 Option3=Rect(0,380,400,125)
 Option4=Rect(425,380,400,125)
+option_boxes=[Option1,Option2,Option3,Option4]
 def draw():
     screen.fill(color="black")
     screen.draw.filled_rect(Welcome,"orange")
@@ -26,6 +28,10 @@ def draw():
     screen.draw.textbox(str(timer),Timer,color="white",shadow=(0.5,0.5),scolor="black")
     screen.draw.textbox("Skip",Skip,color="white",angle=90,shadow=(0.5,0.5),scolor="blue")
     screen.draw.textbox(qu[0],Question,color="white")
+    screen.draw.textbox(qu[1],Option1,color="white")
+    screen.draw.textbox(qu[2],Option2,color="white")
+    screen.draw.textbox(qu[3],Option3,color="white")
+    screen.draw.textbox(qu[4],Option4,color="white")
 def update():
     Welcome.x=Welcome.x-2
     if Welcome.right<0:
@@ -43,23 +49,26 @@ def changing_time():
     
     else:
         timer=0
-file_data=[]
-file="C:/Users/janur/Downloads/Microsoft VS Code/jetlearn/questions.txt"
-def data_read():
-    global file_data
-    opened=open(file,"r")
-    for i in opened:
-        file_data.append(i)
-        print(i)
-    opened.close()
-    print(file_data)
+file_data=["What is the capital of Sri Lanka?,Colombo,Jaffna,California,London,1","What is the only U.S. state that can be typed using only one row of a standard QWERTY keyboard?,California,Alaska,Texas,New York,2","Which ear did Vincent Van Gogh cut off?,Left ear,right ear,neither,both,1","What is the only mammal that can fly?,Aardvark,Baboon,Camel,Bat,4","What was the first toy advertised on TV?,Barbie dolls,Lego sets,Mr. Potato Head,G.I. Joe,3","What is the capital of France?,Marseille,Paris,Nice,Lyon,2"]
+
+
 def read_q():
     random.shuffle(file_data)
     print(file_data)
     return file_data.pop(0).split(",")
 
 
-data_read()
+
+def on_mouse_down(pos):
+    index=1
+    global score
+    for i in option_boxes:
+        if i.collidepoint(pos):
+            if index is int(qu[5]):
+                score=score+1
+                print(score)
+        index=index+1
+
 qu=read_q()
 
 
