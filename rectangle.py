488413ce80cb36f39ce1bcdcd4db3d8c9126381d
Aarushan-JetLearner <taarushan@gmail.com>
2026-01-20 19:46:23 +0000

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -4,7 +4,20 @@ screen=pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Rectangle")
 screen.fill("blue")
 pygame.display.update()
+class Rect():
+    def __init__(self,color,dimension):
+        self.dimension=dimension
+        self.color=color
+    def draw(self):
+        pygame.draw.rect(screen,self.color,self.dimension)
+Object=Rect("white",(0,0,100,50))
+Object.draw()
+Object2=Rect("black",(150,150,100,50))
+Object2.draw()
+pygame.display.update()
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
+
+
