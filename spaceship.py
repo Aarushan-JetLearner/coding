882d36001fb8e09d925ba0918367346c76494ace
Aarushan-JetLearner <taarushan@gmail.com>
2026-02-24 19:59:10 +0000

    Add files via upload

--- spaceship.py
+++ spaceship.py
@@ -15,7 +15,7 @@ red_spaceship=pygame.transform.scale(red_spaceship,(spaceship_width,spaceship_he
 red_spaceship=pygame.transform.rotate(red_spaceship,90)
 red_spaceship_health=10
 mid_rect=pygame.Rect(500,0,10,500)
-def drawing(Rect,Rect2,yellow_spaceship_health,red_spaceship_health):
+def drawing(Rect,Rect2,yellow_spaceship_health,red_spaceship_health,bullet_yellow):
     screen.blit(space,(0,0))
     screen.blit(red_spaceship,(Rect.x,Rect.y))
     screen.blit(yellow_spaceship,(Rect2.x,Rect2.y))
@@ -24,12 +24,35 @@ def drawing(Rect,Rect2,yellow_spaceship_health,red_spaceship_health):
     screen.blit(yellow_text,(250,50))
     red_text=font.render("Health="+str(red_spaceship_health),False,"white")
     screen.blit(red_text,(750,50))
+    for i in bullet_yellow:
+        pygame.draw.rect(screen,"yellow",i)
+    for i in bullet_red:
+        pygame.draw.rect(screen,"red",i)
     pygame.display.update()
+def move_bullets(bullet_red,bullet_yellow,Rect,Rect2):
+    for i in bullet_red:
+        i.x=i.x-10
+        if Rect2.colliderect(i):
+            bullet_red.remove(i)
+        elif i.x<0:
+            bullet_red.remove(i)
 
+    for i in bullet_yellow:
+        i.x=i.x+10
+        if Rect.colliderect(i):
+            bullet_yellow.remove(i)
+        elif i.x>1000:
+            bullet_yellow.remove(i)
+
+    
+bullet_red=[]
+bullet_yellow=[]
 Rect=pygame.Rect(750,250,spaceship_width,spaceship_height)
 Rect2=pygame.Rect(250,250,spaceship_width,spaceship_height)
+clock=pygame.time.Clock()
 while True:
-    drawing(Rect,Rect2,yellow_spaceship_health,red_spaceship_health)
+    clock.tick(60)
+    drawing(Rect,Rect2,yellow_spaceship_health,red_spaceship_health,bullet_yellow)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -42,5 +65,22 @@ while True:
                 Rect.y=Rect.y-10
             elif event.key==pygame.K_DOWN and Rect.y+spaceship_height<500:
                 Rect.y=Rect.y+10
+            elif event.key==pygame.K_a and Rect2.x>0:
+                Rect2.x=Rect2.x-10
+            elif event.key==pygame.K_d and Rect2.x+spaceship_width<500:
+                Rect2.x=Rect2.x+10
+            elif event.key==pygame.K_w and Rect2.y>0:
+                Rect2.y=Rect2.y-10
+            elif event.key==pygame.K_s and Rect2.y+spaceship_height<500:
+                Rect2.y=Rect2.y+10
+            elif event.key==pygame.K_LCTRL and len(bullet_yellow)<3:
+                bullet=pygame.Rect(Rect2.x+spaceship_width,Rect2.y+spaceship_height/2,2,3)
+                bullet_yellow.append(bullet)
+            elif event.key==pygame.K_RCTRL and len(bullet_red)<3:
+                bullet2=pygame.Rect(Rect.x,Rect.y+spaceship_height/2,10,3)
+                bullet_red.append(bullet2)
+            move_bullets(bullet_red,bullet_yellow,Rect,Rect2)
+
+
 
     
