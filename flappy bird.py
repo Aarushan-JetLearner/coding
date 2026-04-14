5adf2b6f070a51a495bcc4f0e75e88ab4ab8093a
Aarushan-JetLearner <taarushan@gmail.com>
2026-04-14 20:06:53 +0100

    Add files via upload

--- flappy bird.py
+++ flappy bird.py
@@ -7,9 +7,16 @@ ground=pygame.image.load(r"C:\Users\User\OneDrive\Pro game developer\ground.png"
 ground=pygame.transform.scale(ground,(1000,50))
 screen.blit(bg,(0,0))
 groundx=0
+game_over=False
+flying=False
 class bird(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
+        self.count=0
+        self.index=0
+        self.vel=0
+        self.clicked=False
+
         self.images=[]
         image=pygame.image.load(r"C:\Users\User\OneDrive\Pro game developer\bird1.png")
         image2=pygame.image.load(r"C:\Users\User\OneDrive\Pro game developer\bird2.png")
@@ -17,20 +24,55 @@ class bird(pygame.sprite.Sprite):
         self.images.append(image)
         self.images.append(image2)
         self.images.append(image3)
-        self.image=self.images[0]
+        self.image=self.images[self.index]
         self.rect=self.image.get_rect()
+        self.rect.center=x,y
+    def update(self):
+        if flying==True:
+
+            self.vel=self.vel+5
+            if self.vel>=10:
+                self.vel=10
+            if self.rect.bottom<=450:
+                self.rect.y=self.rect.y+self.vel
+
+        self.count=self.count+1
+        self.store=5
+        if self.count>=self.store:
+            self.count=0
+            self.index=self.index+1
+            if self.index>2:
+                self.index=0
+        self.image=self.images[self.index]
+        
+
+
 bird_group=pygame.sprite.Group()
 bird_object=bird(250,250)
 bird_group.add(bird_object)
 
+
 while True:
+    screen.blit(bg,(0,0))
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-    groundx=groundx-1
+        elif event.type==pygame.MOUSEBUTTONDOWN and flying==False and game_over==False:
+            flying=True
     screen.blit(ground,(groundx,450))
-    if abs(groundx)>20:
-        groundx=0
+    if game_over==False:
+        groundx=groundx-1
+        if abs(groundx)>20:
+           groundx=0
+        
+    if bird_object.rect.bottom>=450:
+        game_over=True
+        flying=False
+
     bird_group.draw(screen)
+    
+    
+    bird_object.update()
 
     pygame.display.update()
