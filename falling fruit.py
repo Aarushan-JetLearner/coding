6ee358b6fa9d097bac9d1963c07afe7abda6d93d
Aarushan-JetLearner <taarushan@gmail.com>
2026-04-30 19:19:32 +0100

    Add files via upload

--- falling fruit.py
+++ falling fruit.py
@@ -5,11 +5,13 @@ bg=pygame.image.load(r"C:\Users\User\OneDrive\Pro game developer\bgfalling fruit
 bg=pygame.transform.scale(bg,(1000,500))
 screen.blit(bg,(0,0))
 class basket(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         super().__init__()
         self.image=pygame.image.load(r"C:\Users\User\OneDrive\Pro game developer\basket.png")
         self.image=pygame.transform.scale(self.image,(75,75))
         self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
 class fruits(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -29,27 +31,44 @@ class fruits(pygame.sprite.Sprite):
         self.images.append(banana)
         self.images.append(mango)
         self.images.append(orange)
-        self.image=self.images[0]
+        self.image=random.choice(self.images)
         self.rect=self.image.get_rect()
+        self.rect.x=random.randint(0,1000)
+    def update(self):
+        if self.rect.y<500:
+            self.rect.y=self.rect.y+20
+
         
+screen.blit(bg,(0,0))       
 group_basket=pygame.sprite.Group()
 group_fruits=pygame.sprite.Group()
-object_basket=basket()
+object_basket=basket(450,425)
 group_basket.add(object_basket)
 group_basket.draw(screen)
 
 object_fruits=fruits()
 group_fruits.add(object_fruits)
-
+pygame.display.update()
 
 pygame.display.update()
+
 while True:
+    fruits()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
 
             pygame.quit()
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT and object_basket.rect.x>0:
+                object_basket.rect.x=object_basket.rect.x-30
+                screen.blit(bg,(0,0))
+            elif event.key==pygame.K_RIGHT and object_basket.rect.x<925:
+                object_basket.rect.x=object_basket.rect.x+30
+                screen.blit(bg,(0,0))
+         
             
         group_basket.draw(screen)
         group_fruits.draw(screen)
+        
         pygame.display.update()
         
